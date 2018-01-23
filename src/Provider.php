8f79b1724dfd1b7845c7544dff38739a91c439cb
Ryan Durham <ryan@stagerightlabs.com>
2018-01-23 10:53:20 -0800

    Register the import and export commands in the package service provider.

--- src/Provider.php
+++ src/Provider.php
@@ -1,9 +1,25 @@
 <?php
 
-namespace MigrationWrangler;
+namespace SRLabs\MigrationWrangler;
 
 use Illuminate\Support\ServiceProvider;
+use SRLabs\MigrationWrangler\Commands\MigrationsImporter;
+use SRLabs\MigrationWrangler\Commands\MigrationsExporter;
 
 class Provider extends ServiceProvider
 {
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                MigrationsExporter::class,
+                MigrationsImporter::class,
+            ]);
+        }
+    }
 }
